1ac222ff4823213a9977e91e40869f88244df0f3
Gnanasekar <532968@cognizant.com>
2017-08-03 10:43:57 +0530

    change to original file

--- app/components/news-tile.js
+++ app/components/news-tile.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	
-
+/*fdfdf*/
 		 actions: {
     sendDataToController(eachNewsId) {
       this.sendAction('sendData',eachNewsId);
